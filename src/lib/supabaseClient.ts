38e9ea16095d19ffed2aba8de28703d42118082c
collinskani36 <collinskani36@gmail.com>
2026-07-02 10:44:21 +0300

    updated supabase

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -5,3 +5,5 @@ export const supabase = createClient<Database>(
   import.meta.env.VITE_SUPABASE_URL,
   import.meta.env.VITE_SUPABASE_ANON_KEY,
 );
+
+if (typeof window !== "undefined") (window as any).supabase = supabase;
